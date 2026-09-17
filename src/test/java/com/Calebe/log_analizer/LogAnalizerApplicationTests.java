73cba66c2d036af461c7f721a7c710660c8396f9
ANOSH <calebe11anos@gmail.com>
2026-09-17 11:20:07 -0300

    chore: add missing files

---
+++ src/test/java/com/Calebe/log_analizer/LogAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Calebe.log_analizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
