9ad71fb91b95cd0da5f8d7a593860a8ab04914b5
ANOSH <calebe11anos@gmail.com>
2026-09-18 17:10:43 -0300

    feat: add test about Database

---
+++ src/test/java/com/Calebe/log_analizer/MitreTacticRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.Calebe.log_analizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import repository
+import com.Calebe.log_analizer.repository.MitreTacticRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class MitreTacticRepositoryTests {
+
+    private static final Logger log = LoggerFactory.getLogger(MitreTacticRepositoryTests.class);
+
+    @Autowired
+    private MitreTacticRepository repository;
+
+    @Test
+    void shouldSaveATactic() {
+        MitreTactic tactic = repository.save(
+            new MitreTactic("TA0006", "Credential Access", "Attempts to obtain credentials")
+        );
+
+        log.info("Saved: {}", tactic);
+
+        assertThat(tactic.getId()).isNotNull();
+        assertThat(tactic.getTacticId()).isEqualTo("TA0006");
+        assertThat(tactic.getName()).isEqualTo("Credential Access");
+    }
+}
